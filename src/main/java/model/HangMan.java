d63af362caa28b19144f59b595310540f9022c02
linaresjuanjose <juan.linares@smartmatic.com>
2013-12-14 15:04:52 -0500

    HangMan 2

--- src/main/java/model/HangMan.java
+++ src/main/java/model/HangMan.java
@@ -2,24 +2,56 @@ package model;
 
 public class HangMan {
 
+	
+	public int MAX_TRIES = 4;
+	
+
+	char[] letters;
+	char[] lettersDisplay;
+	char[] lettersUsed;
+	int lettersUsedCount = 0;
+	
+	int lettersRemaining = 0;
+	int currentTries = 0; 
+		
 	String gameWord;
 
-	int retryNum, status;
+	int  status;
 
 	// pierde, gana, repetido, valido, invalido
 	// -1, 0, 1, ,2, 3
 
 	public HangMan(String word) {
 		gameWord = word;
-		retryNum = gameWord.length();
+		//retryNum = gameWord.length();
+				
+		
+		lettersUsed = new char[1000];		
+		letters = new char[word.length()];
+		lettersDisplay = new char[word.length()];
+		lettersRemaining = word.length();
+		for (int i = 0; i < lettersDisplay.length; i++) {
+			lettersDisplay[i]='-';
+		}
+		
 	}
 
 	public boolean checkCharacter(char c) {
-		return (gameWord.lastIndexOf(c) >= 0);
+		boolean result = false;
+
+		for (int i = 0; i < letters.length; i++) {
+			if(c==letters[i])
+			{
+				result = true;
+				lettersDisplay[i] = c;
+				lettersRemaining--;
+			}
+		}	
+		return result;
 	}
 
 	public boolean isWinner() {
-		return false;
+		return (lettersRemaining == 0);
 	}
 
 	public int play(char c) {
@@ -33,7 +65,7 @@ public class HangMan {
 				return 2;
 			}
 		} else {
-			retryNum--;
+			currentTries++;
 			if (isLoser()) {
 				return -1;
 			} else {
@@ -43,10 +75,27 @@ public class HangMan {
 	}
 
 	public boolean isLoser() {
-		return (retryNum == 0);
+		return (MAX_TRIES == currentTries);
 	}
 
 	public boolean isUsedLetter(char c) {
-		return false;
+
+		boolean result = false;
+		
+		for (int i = 0; i < this.lettersUsed.length; i++) {
+			if(c==this.lettersUsed[i])
+			{
+				result = true;
+				break;
+			}
+		}	
+		
+		return result;	
+	}
+	
+
+	public String GetDiscoveredLetters()
+	{
+		return new String(this.lettersDisplay);
 	}
 }
